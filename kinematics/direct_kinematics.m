017b135d25ddc4b672858e6832ee996833a9d27b
Patrik Kukic <patrikkukic@gmail.com>
2023-06-05 16:01:30 +0200

    added kinematics

---
+++ kinematics/direct_kinematics.m
@@ -0,0 +1,49 @@
+function location = direct_kinematics(angle_motor_1, angle_motor_2, angle_motor_3)
+
+    a1 = 87.3;
+    a2 = 177.0;
+    a3 = 188.0;
+    a4 = 61.3;
+
+    q1 = deg2rad(angle_motor_1);
+    q2 = deg2rad(angle_motor_2);
+    q3 = deg2rad(angle_motor_3);
+
+    T01 = zeros(4, 4);
+    T01(1, :) = [0 cos(q1) -sin(q1) 0];
+    T01(2, :) = [0 sin(q1) cos(q1) 0];
+    T01(3, :) = [1 0 0 a1];
+    T01(4, :) = [0 0 0 1];
+
+    T12 = zeros(4, 4);
+    T12(1, :) = [cos(q2) -sin(q2) 0 a2 * cos(q2)];
+    T12(2, :) = [sin(q2) cos(q2) 0 a2 * sin(q2)];
+    T12(3, :) = [0 0 1 0];
+    T12(4, :) = [0 0 0 1];
+
+    T23 = zeros(4, 4);
+    T23(1, :) = [cos(q3) -sin(q3) 0 a3 * cos(q3)];
+    T23(2, :) = [sin(q3) cos(q3) 0 a3 * sin(q3)];
+    T23(3, :) = [0 0 1 0];
+    T23(4, :) = [0 0 0 1];
+
+    T03 = T01 * T12 * T23;
+    q4 = acos(abs(T03(3, 1)));
+
+    T3T = zeros(4, 4);
+    T3T(1, :) = [-sin(q4) 0 cos(q4) a4 * cos(q4)];
+    T3T(2, :) = [cos(q4) 0 sin(q4) a4 * sin(q4)];
+    T3T(3, :) = [0 1 0 0];
+    T3T(4, :) = [0 0 0 1];
+
+    T0T = T03 * T3T;
+    disp(T0T);
+
+    x = T0T(1, 4);
+    y = T0T(2, 4);
+    z = T0T(3, 4);
+
+    % Return the location as a triple of three floats
+    location = [x, y, z];
+    
+end
---
+++ kinematics/inverse_kinematics.m
@@ -0,0 +1,27 @@
+function angles = inverse_kinematics(X, Y, Z)
+    
+    a1 = 87.3;
+    a2 = 177.0;
+    a3 = 188.0;
+    a4 = 61.3;
+
+    q1 = atan2(Y, X);
+
+    h1 = X * cos(q1) + Y * sin(q1);
+    h2 = Z - a1 + a4;
+    dist2 = h1^2 + h2^2;
+
+    q3 = acos((dist2 - a2^2 - a3^2) / (2 * a2 * a3));
+
+    s2 = (h1 * (a2 + a3 * cos(q3)) - a3 * sin(q3) * h2) / dist2;
+    c2 = (h2 * (a2 + a3 * cos(q3)) + a3 * sin(q3) * h1) / dist2;
+
+    q2 = atan2(s2, c2);
+
+    angle1 = rad2deg(q1);
+    angle2 = rad2deg(q2);
+    angle3 = rad2deg(q3);
+
+    angles = [angle1, angle2, angle3];
+
+end
