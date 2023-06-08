9f662a0540856b2bb0b5117b8b1614561c19ea10
Matej Balog <matej.balog@hotmail.com>
2023-06-08 15:41:43 +0200

    transformations are okay now

--- kinematics/direct_kinematics.m
+++ kinematics/direct_kinematics.m
@@ -1,9 +1,9 @@
 function location = direct_kinematics(angle_motor_1, angle_motor_2, angle_motor_3)
 
-    a1 = 87.3;
+    a1 = 88.0;
     a2 = 177.0;
-    a3 = 188.0;
-    a4 = 61.3;
+    a3 = 177.0;
+    a4 = 63.0;
 
     q1 = deg2rad(angle_motor_1);
     q2 = deg2rad(angle_motor_2);
@@ -15,11 +15,17 @@ function location = direct_kinematics(angle_motor_1, angle_motor_2, angle_motor_
     T01(3, :) = [1 0 0 a1];
     T01(4, :) = [0 0 0 1];
 
+    disp('T01');
+    disp(T01);
+
     T12 = zeros(4, 4);
     T12(1, :) = [cos(q2) -sin(q2) 0 a2 * cos(q2)];
     T12(2, :) = [sin(q2) cos(q2) 0 a2 * sin(q2)];
     T12(3, :) = [0 0 1 0];
     T12(4, :) = [0 0 0 1];
+
+    disp('T02');
+    disp(T01 * T12);
 
     T23 = zeros(4, 4);
     T23(1, :) = [cos(q3) -sin(q3) 0 a3 * cos(q3)];
@@ -28,7 +34,11 @@ function location = direct_kinematics(angle_motor_1, angle_motor_2, angle_motor_
     T23(4, :) = [0 0 0 1];
 
     T03 = T01 * T12 * T23;
-    q4 = acos(abs(T03(3, 1)));
+    q4 = acos(-(T03(3, 1)));
+    disp(q4);
+    
+    disp('T03');
+    disp(T03);
 
     T3T = zeros(4, 4);
     T3T(1, :) = [-sin(q4) 0 cos(q4) a4 * cos(q4)];
@@ -37,6 +47,7 @@ function location = direct_kinematics(angle_motor_1, angle_motor_2, angle_motor_
     T3T(4, :) = [0 0 0 1];
 
     T0T = T03 * T3T;
+    disp('T0T');
     disp(T0T);
 
     x = T0T(1, 4);
