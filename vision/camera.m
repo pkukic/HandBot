67ccac9c583529d23f877b201ae92494e5deb14d
Matej Balog <matej.balog@hotmail.com>
2023-06-08 18:31:15 +0200

    referented angles 180

--- vision/camera.m
+++ vision/camera.m
@@ -1,7 +1,7 @@
 function [p1, p2, p3, p4, p5, p6] = camera(color_order, cameraParams, R, t)
     % color_order is like ['g', 'y', 'b']
     % take the image
-    camera_info = imaqhwinfo('winvideo', 1);
+    camera_info = imaqhwinfo('winvideo', 2);
     video_obj = videoinput('winvideo', camera_info.DeviceID, camera_info.DefaultFormat);
     video_obj.ReturnedColorSpace = 'rgb';
     f = figure('Visible', 'off'); 
